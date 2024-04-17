eed06b8783f792f2760332848a23ce138edcc47f
chamcham0707 <cheamin6306@naver.com>
2024-04-17 10:41:26 +0900

    main merge

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const circleElements = document.querySelectorAll('.circle');
+  const squareElements = document.querySelectorAll('.square');
+
+  circleElements.forEach(circle => {
+    circle.addEventListener('mouseenter', () => {
+      const pTag = circle.querySelector('p');
+      pTag.classList.add('hidden');
+    });
+    circle.addEventListener('mouseleave', () => {
+      const pTag = circle.querySelector('p');
+      pTag.classList.remove('hidden');
+    });
+  });
+
+  squareElements.forEach(square => {
+    square.addEventListener('mouseenter', () => {
+      const pTag = square.querySelector('p');
+      pTag.classList.add('hidden');
+    });
+    square.addEventListener('mouseleave', () => {
+      const pTag = square.querySelector('p');
+      pTag.classList.remove('hidden');
+    });
+  });
+
+});
